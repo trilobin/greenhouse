080a32244ba831aa8e1c00e4b9b931ef60c1f255
trilobin <98662164+trilobin@users.noreply.github.com>
2022-02-16 09:47:46 +0200

    Version 10. PI-controller, stable at 70% RH

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 """
-Version 6. PID-regulator
+Greenhouse project - Adaptive humidification
+Version 9. PID-regulator (Only P and I components in use now).
 
-Greenhouse project
-Adaptive humidification. Discrete PID-regulator.
+E.g.
+https://www.amazon.com/Humidity-Controller-Inkbird-Humidistat-Pre-wired/dp/B01J1E5LWM
+This one only turns on and turns off humidifier at defined values (it also controls
+a dehumidifier if you have one). There is no actual control to how high or low
+the humidity gets.
+Trying the same approach resulted in fluctuations of +-20% RH.
 
-At start of humidification
-If humidity too high but decreasing (projection close to correct) (since last cycle): leave as is
-If humidity too high and constant, decrease humidification time (or increase break? maybe both start+stop=60)
-If humidity too high and increasing, decrease humidification time
-If humidity too low but increasing (projection getting close to correct): leave as is
-If humidity too low and constant, increase humidification time
-If humidity too low and decreasing, increase humidification time
+Let's try a PI controller without the derivative term. And maybe set the
+tuning parameters while aiming for 70% RH (instead of 95% RH) to prevent
+the process from hitting 99.9% RH and flooding the space.
 
-Control based on projected error which is calculated from current error,
-derivative of error, and second derivative of error.
+Maybe make the algorithm increase humidity in steps. If we start from 20% RH,
+then first reach 50% and stabilize, then 70%, then 80%, then 90%, then 95%?
+This would to some extent stabilize the system while trying to reach higher
+values at the same time preventing the system from flooding. Flooding is
+problematic as it is a discontinuous point from the viewpoint of the algorithm.
 
-Perhaps make changes slow when approaching the humidity limit from below
-but large when exceeding it. This would serve to slowly build up to the desired
-humidity and then keeping it there, rather than having it run away. Maybe.
-
-Alternatively binary search could be used to look for optimal humidification time.
-It would need to be "restarted" or somehow backtracked from time to time.
+Reinforcement learning (with a neural net) would probably be quite good for this,
+although collecting training data could take time.
 """
 
 import time
@@ -31,10 +31,7 @@ from DHT22 import DHT22
 def log_details(current_humidity,
                 humidification_rate,
                 error,
-                derivative,
-                second_derivative,
-                projected_error,
-                change):
+                integral_err):
     """
     Function for logging details
     
@@ -44,11 +41,7 @@ def log_details(current_humidity,
      large part of the cycle the humidifier stays on.
     error (float): Last measured deviation from target
      humidity.
-    derivative (float): Estimated change rate in humidity
-     in a 10 second cycle.
-    second_derivative (float): Estimated change of change
-     (derivative) in last two cycles (20 seconds).
-    change (float): Last adjustement to humidification rate.
+    integral (float): Accumulated error
     """
     try:
         # First check for existence of file. If it exists,
@@ -56,44 +49,79 @@ def log_details(current_humidity,
         # the Pico to your computer and it does not go into
         # REPL-model).
         with open('log.csv', 'a') as handle:
-            handle.write('"{}","{}","{}","{}","{}","{}","{}"\n'.format(current_humidity,
-                                                                       humidification_rate,
-                                                                       error,
-                                                                       derivative,
-                                                                       second_derivative,
-                                                                       projected_error,
-                                                                       change))
+            handle.write('"{}","{}","{}","{}"\n'.format(current_humidity,
+                                                        humidification_rate,
+                                                        error,
+                                                        integral_err))
     except:
         # Create file for logging:
         with open('log.csv', 'w') as handle:
-            handle.write('"Humidity","ON/OFF","ON-time","Cycle time"\n')
-            handle.write('"{}","{}","{}","{}","{}","{}","{}"\n'.format(current_humidity,
-                                                                       humidification_rate,
-                                                                       error,
-                                                                       derivative,
-                                                                       second_derivative,
-                                                                       projected_error,
-                                                                       change))
+            handle.write('"Humidity","Rate","Error","Integral error"\n')
+            handle.write('"{}","{}","{}","{}"\n'.format(current_humidity,
+                                                        humidification_rate,
+                                                        error,
+                                                        integral_err))
 
 
-def control_humidity(verbose=False):
+def linear_regression(t, y):
     """
-    A function for controlling relay (and humidifier). Nothing
-    simple about this anymore.
+    Ordinary least squares regression (univariate).
+    Only the coefficient is needed.
     
-    10 second cycle (humidifier on & off)
-    Adjusting settings every 10 cycles
+    Args:
+    t (float): Independent variable
+    y (float): Dependent variable
+    
+    Notes:
+    f(t) = coef * t + constant (\approx y)
+    y = coef * t + constant + err
+    err ~ N(0,.)
+    min_{coef, constant) sum((f(t)-y)^2)
+
+    Formulas from: https://datacadamia.com/data_mining/simple_regression#ordinary_least_squares_method
+    -Perhaps the problem would be better modeled by a 2nd degree polynomial?
+    """
+    mean_t = sum([item for item in t]) / len(t)
+    mean_y = sum([item for item in y]) / len(y)
+    coef = sum([(item_1 - mean_t)*(item_2 - mean_y) for item_1, item_2
+                in zip(t, y)]) /\
+                sum([(item - mean_t)**2 for item in t])
+    constant = mean_y - coef * mean_t
+    return coef, constant
+
+
+class FakeSensor():
+    """
+    Fake temperature and humidity class for
+    testing purposes.
+    """
+    def __init__(self):
+        # Does not need to do anything.
+        pass
+
+    def read(self):
+        # Make up some reasonable number pair
+        # Could use random numbers for more realistic
+        # testing.
+        return 23, 55
+
+
+def control_humidity(verbose=False, test=False):
+    """
+    A function for controlling relay (and humidifier). Currently the
+    mist from a humidifier is directed into a chamber with a little
+    water on the bottom and the stones of a bubbler. The bubbler is
+    on for humidification_rate * cycle time, and the humidifier is
+    on for half of that.
+
+    Currently using 10 second cycles. The devices are on for some
+    fraction of that cycle and are reset at the end of it.
 
     Args:
     verbose (bool): Prints out metrics every cycle
-    
-    Notes:
-    Perhaps implement a parabola with zero-point at humidity_target.
-     That could serve as an adaptive coefficient?
-     Essentially some kind of variable coefficient for change
-     could stabilize the system.
-     Currently there is a "hinge-loss" -like solution for this.
-     
+    test (bool): If True, uses a fake generator to generate sensor
+     readings. For testing purposes (without device).
+         
     Perhaps estimate derivative by fitting f(t) \approx c * t + err and use
      c as estimate for derivative. Using three to five points would provide
      a more stable estimate this way. Do something similar for the second
@@ -105,30 +133,45 @@ def control_humidity(verbose=False):
      high for a long time. Perhaps just wait it out? The PID-controller seems
      to handle it reasonably well.
 
-    Perhaps limit change to 15-20% per minute? Could prevent the humidifier
-     from saturating the room.
-    With c_d 0.4 and c_p 5.0, min 10, max 40, the humidity fluctuated between
-     71% and 97%.
+    If the humidity is much too low, maybe make the humidifier climb up to the
+    set target humidity in steps? E.g. starting at 20% aim for 50%, when
+    that is reached, change parameters to 70% etc to reach the desired
+    humidity in a more controlled fashion.
+    
+    Perhaps separate control of airpump and humidifier. Airpump running time
+    would be defined by desired FAE and humidifier running time would then
+    be a function of both airpump running time and target humidity. The
+    humidifier would still be controlled by a PI-controller, though.
     """
     # Humidifier runs in short cycles to keep humidity constant better.
     cycle_time = 10  # seconds
     humidification_rate = 20  # % of the cycle
     # Can adjust the cycle times later if needed.
-    c_e = 1.0  # Coefficient for error
-    c_d = 1.0  # Coefficient for derivative
-    c_dd = 0.7  # Coefficient for second derivative
-    # A more accurate estimate of the second derivative would be super
-    # useful, but the sensor will not do that. So it might be better
-    # to just use less weight for this. On average, it will still be
-    # allright.
-    n_loops = 10  # Number of loops between adjusting settings
+    c_i = 0.06  # Coefficient for integral. 0.01 Implies that a 40%
+    # change in the previous 1000 seconds results in a 4 % increase
+    # in humidification time.
+    # c_i 0.01 and c_p 1.0 produced a fairly stable humidity around
+    # 70% RH (+3-2) and reached equilibrium in one hour. Trying
+    # c_i 0.06 to perhaps reach equilibrium faster. c_p might also
+    # work better with a higher value to more aggressively counter
+    # fluctuations.
+    c_p = 1.0  # Coefficient for error. 0.25 implies that an error of
+    # 100% would produce a 40% humidification rate next cycle.
+    c_d = 0.0  # Coefficient for derivative
+    # As we are not estimating derivatives, updating settings every
+    # cycle or every 2 cycles seems reasonable.
+    # Must be at least 2 if we want to estimate derivative.
+    n_loops = 2  # Number of loops between adjusting settings
 
     # The actual target
-    humidity_target = 95.0  # % RH. This is the target humidity.
+    humidity_target = 70.0  # % RH. This is the target humidity.
     
-    # relay_1 = Pin(6, Pin.OUT)  # This is the relay above the usb-port
+    relay_1 = Pin(6, Pin.OUT)  # This is the relay above the usb-port
     relay_2 = Pin(7, Pin.OUT)  # This is the relay opposite from the usb-port
-    sensor = DHT22(Pin(3, Pin.IN, Pin.PULL_UP))
+    if not test:
+        sensor = DHT22(Pin(3, Pin.IN, Pin.PULL_UP))
+    else:
+        sensor = FakeSensor()
     # Sleep 2 seconds to ensure sensor is ready.
     time.sleep_ms(2000)
     if cycle_time < 2:
@@ -139,84 +182,71 @@ def control_humidity(verbose=False):
     # Log details before starting loop:
     temperature, humidity = sensor.read()
     # Store an empty line for "restart".
-    log_details(humidity, 0, 0, 0, 0, 0, 0)
+    log_details(humidity, 0, 0, 0)
+    
+    # Initialize list
+    # 25% RH is somewhat typical indoor air humidity
+    humidity_list = [25.0 for _ in range(n_loops)]
+    integral_err = 0.0
 
-    humidity_list = [0.0, 0.0, 0.0]
     while True:
         # Continuous loop
-
-        # Proportional part:
+        # Proportional error:
         error =  humidity - humidity_target
-        # Derivative (change during last 10 seconds)
-        derivative = humidity_list[-1] - humidity_list[-2]
-        # Second derivative
-        second_derivative = derivative - (humidity_list[-2] - humidity_list[-3])
-        # Projection based on these three for the end of next cycle:
-        # Project over 10 cycles, one full loop:
-        # Could dampen the effect of derivative and especially second derivative.
-        # n_loops * d is the amount of error the derivative will introduce in the same
-        # number of loops. factorial(n_loops) * s-d is the amount of error the second
-        # derivative will introduce in n_loops.
-        # factorial(n_loops) would hypothetically be correct, but factorial(10)
-        # =3628800... I.e. it would make the system hyper sensitive (and unstable).
-        projected_error = c_e * error +\
-                          (c_d * n_loops * derivative +
-                           + c_dd * n_loops * second_derivative)
-        # This approximation is rather ad hoc:
-        # Could e.g. be averages from the last 10 iterations
-        #humidification_velocity = humidity / humidification_rate
-        humidification_velocity = 4  # Constant based on your setup.
-        # Flip sign becasue we want to fix the error
-        change = -projected_error / humidification_velocity
-        # Hinge-loss:
-        if humidity < humidity_target and change > 0:
-            # Just make half of the needed change. Approach
-            # desired value slowly.
-            change *= 0.5
-        elif humidity > humidity_target and change < 0:
-            change *= 4
-        humidification_rate += change
-        # Reset lists:
+        # Derivative as regression coefficient (now equal to difference
+        # between two last measure points).
+        tmp = [humidity_list[i] - humidity_list[j] for i, j in
+               zip(range(1, 2), range(0, 1))]
+               #zip(range(6, 10), range(5, 9))]
+        # The regression approach is meant to remove random fluctuations
+        # (both fluctuations in the air and in measurement accuracy).
+        coef, _ = linear_regression([0, 1, 2], humidity_list[-3:])
+        derivative = coef
+
+        # Integral error:
+        tmp = [item - humidity_target for item in humidity_list]
+        integral_err += sum(tmp)
+        # The min-max is meant to clip the error at the point where
+        # the integral part would keep the humidifier on for 100% of
+        # the time or 0% of the time. If the setup contained a dehumidifier,
+        # and the target humidity was below exchange air humidity, then
+        # positive values should also be allowed.
+        integral_err = min(0.0, max(-1 / c_i * 100, integral_err))
+
+        # Combine the results (PI-control):
+        humidification_rate = - integral_err * c_i - error * c_p
+        #Reset lists:
         humidity_list = []
 
-        humidification_rate = max(5.0, min(50.0, humidification_rate))
-        # With min 5 and max 50%, the humidity stayed in 88-98%.
-
+        humidification_rate = max(0.0, min(100.0, humidification_rate))
 
         if verbose:
             print("=" * 40)
             print("Humidity: {}% RH".format(humidity))
-            print("Temperature: {} C".format(temperature))
+            print("Proportional error: {}".format(error))
+            print("Integral error: {}".format(integral_err))
             print("Humidification rate: {}% of cycle".format(humidification_rate))
             print("Cycle time: {} s".format(cycle_time))
-            try:
-                print("Min humidity in cycle: {}% RH".format(min_cycle_humidity))
-                print("Max humidity in cycle: {}% RH".format(max_cycle_humidity))
-            except:
-                pass
-            print("-" * 40)
-            print("Error: {}".format(error))
-            print("Derivative: {}".format(derivative))
-            print("Second derivative: {}".format(second_derivative))
-            print("Projected error: {}".format(projected_error))
-            print("Change: {}".format(change))
-            print("New humidification_rate: {}".format(humidification_rate))
-            print("-" * 40)
 
         for j in range(n_loops):
-            # cycle_time is 10 seconds. With 10 iterations, that
-            # makes for 100 seconds.
+            # Settings are re-estimated every n_loops * cycle_time.
+            tmp = int(cycle_time * humidification_rate)
             for i in range(int(cycle_time * humidification_rate)):
-                # Turn humidifier on
+                # Keep humidifier on half of the time the air pump
+                # is kept on:
+                if i < tmp * .5:
+                    relay_1.value(1)
+                else:
+                    relay_1.value(0)
                 relay_2.value(1)
                 time.sleep_ms(10)  # 0.01 seconds
             for i in range(int(cycle_time * (100 - humidification_rate))):
+                relay_1.value(0)
                 relay_2.value(0)
                 time.sleep_ms(10)
             # Log details every cycle (every 10 seconds)
             temperature, humidity = sensor.read()
-            log_details(humidity, humidification_rate, error, derivative,
-                        second_derivative, projected_error, change)
+            log_details(humidity, humidification_rate, error, integral_err)
             humidity_list.append(humidity)
             if verbose:
                 print("Humidity: {}".format(humidity))
